3a3498cc9bdf236289549f8b84679a85279460f5
Vloer <borisruwe@hotmail.com>
2023-04-24 16:56:55 +0200

    init

---
+++ KeyCount_dev/Defaults.lua
@@ -0,0 +1,31 @@
+Defaults = {
+    DungeonNamesShort = {
+        AV = "The Azure Vault",
+        RLP = "Ruby Life Pools",
+        HOV = "Halls of Valor",
+        NO = "The Nokhud Offensive",
+        SBG = "Shadowmoon Burial Grounds",
+        COS = "Court of Stars",
+        TJS = "Temple of the Jade Serpent",
+        AA = "Algeth'ar Academy"
+    }
+}
+
+DungeonDefault = {
+    player = "",
+    name = "",
+    party = {},
+    startedTimestamp = 0,
+    completed = false,
+    completedTimestamp = 0,
+    completedInTime = false,
+    timeToComplete = "",
+    time = 0,
+    deaths = 0,
+    keyDetails = {
+        level = 0,
+        affixes = {}
+    },
+    usedOwnKey = false,
+    timeLimit = 0
+}
