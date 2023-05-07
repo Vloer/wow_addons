c0cbab0a06b5210e6dcc9b7d46cd645db9bd1206
Vloer <borisruwe@hotmail.com>
2023-05-07 23:16:57 +0200

    working gui still standalone

--- Gui.lua
+++ Gui.lua
@@ -1,20 +1,26 @@
-Data = {}
 local key = ""
 local value = ""
 local filtertype = "list"
-local executeFilter = function()
-    FilterFunc.print[filtertype](key, value)
-end
 local fillTable = function()
-    Data = FilterFunc[filtertype](key, value)
+    local dungs = FilterFunc[filtertype](key, value)
+    if not dungs then return end
+    local data = PrepareData.list(dungs)
+    st:SetData(data)
     st:Refresh()
+end
+
+local function disableWidgets(setting, widgets)
+    for _, w in ipairs(widgets) do
+        w:SetDisabled(setting)
+    end
 end
 
 AceGUI = LibStub("AceGUI-3.0")
 local frame = AceGUI:Create("Frame")
 frame:SetTitle("KeyCount")
-frame:SetStatusText("Retrieve some data on your mythic+ runs!")
+frame:SetStatusText("Retrieve some data for your mythic+ runs!")
 frame:SetWidth(750)
+frame:SetHeight(420)
 frame:SetCallback("OnClose", function(widget) AceGUI:Release(widget) end)
 frame:SetLayout("Flow")
 
@@ -22,30 +28,40 @@ frame:SetLayout("Flow")
 local dropdownBox = AceGUI:Create("Dropdown")
 dropdownBox:SetLabel("Filter type")
 dropdownBox:SetWidth(100)
-dropdownBox:AddItem("list", "List")
+dropdownBox:AddItem("list", "All data")
 dropdownBox:AddItem("filter", "Filter")
 dropdownBox:AddItem("rate", "Success rate")
-dropdownBox:SetCallback("OnValueChanged", function(widget, event, item) filtertype = item end)
+dropdownBox:SetCallback("OnValueChanged", function(widget, event, item)
+    filtertype = item
+    if item == "list" then
+        disableWidgets(true, FilterWidgets)
+    else
+        disableWidgets(false, FilterWidgets)
+    end
+end)
 dropdownBox:SetValue("list")
 frame:AddChild(dropdownBox)
 
-local editbox = AceGUI:Create("EditBox")
-editbox:SetLabel("Filter key")
-editbox:SetWidth(200)
-editbox:SetCallback("OnEnterPressed", function(widget, event, text) key = text end)
-frame:AddChild(editbox)
+local editboxKey = AceGUI:Create("EditBox")
+editboxKey:SetLabel("Filter key")
+editboxKey:SetWidth(200)
+editboxKey:SetCallback("OnEnterPressed", function(widget, event, text) key = text end)
+editboxKey:SetDisabled(true)
+frame:AddChild(editboxKey)
 
-local editbox = AceGUI:Create("EditBox")
-editbox:SetLabel("Filter value")
-editbox:SetWidth(200)
-editbox:SetCallback("OnEnterPressed", function(widget, event, text) value = text end)
-frame:AddChild(editbox)
+local editboxVal = AceGUI:Create("EditBox")
+editboxVal:SetLabel("Filter value")
+editboxVal:SetWidth(200)
+editboxVal:SetCallback("OnEnterPressed", function(widget, event, text) value = text end)
+editboxVal:SetDisabled(true)
+frame:AddChild(editboxVal)
+
+FilterWidgets = { editboxKey, editboxVal }
 
 local button = AceGUI:Create("Button")
 button:SetText("Show data")
 button:SetWidth(200)
-button:SetCallback("OnClick", executeFilter)
---button:SetCallback("OnClick", fillTable)
+button:SetCallback("OnClick", fillTable)
 frame:AddChild(button)
 
 _G["KeyCountFrame"] = frame.frame
@@ -60,12 +76,10 @@ local columns = {
     { ["name"] = "Deaths",  ["width"] = 55,  ["defaultsort"] = "dsc" },
     { ["name"] = "Affixes", ["width"] = 200, },
 };
-local data = PrepareData.list(GetStoredDungeons())
+
 local ScrollingTable = LibStub("ScrollingTable");
-st = ScrollingTable:CreateST(columns, 12, 16, nil, window);
-st.frame:SetPoint("TOP", window, "CENTER", 0, 46);
+st = ScrollingTable:CreateST(columns, 16, 16, nil, window);
+st.frame:SetPoint("TOP", window, "TOP", 0, -100);
 st.frame:SetPoint("LEFT", window, "LEFT", 15, 0);
 st:EnableSelection(true)
-st:SetData(data)
-st:Refresh()
 frame:SetCallback("OnClose", function() st:Hide() end)
