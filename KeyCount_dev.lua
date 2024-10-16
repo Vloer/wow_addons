55c5886ce04a99a771393fec9b30962f57225641
Vloer <borisruwe@hotmail.com>
2024-10-16 19:03:17 +0200

    Disabled previous season automatic showing

--- KeyCount_dev.lua
+++ KeyCount_dev.lua
@@ -129,7 +129,7 @@ function KeyCount:InitSelf()
         table.copy(self.current, KeyCountDB.current)
     end
     local updateMessage =
-    "·Player lookup screen now supports filtering by season·"
+    "·Disabled showing previous season again since we are one month into TWW S1·"
     C_Timer.After(15, function() KeyCount.util.checkUpdateMessage(updateMessage) end)
     Log("Finished InitSelf")
 end
